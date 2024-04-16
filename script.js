d587dc6fb0c02d2153b9f96331480b48af4d7e79
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-04-16 22:11:32 +0530

    Refactor script.js to improve code readability and add support for line breaks on Enter key press

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn')
 let notes = document.querySelectorAll(".input-box")
 
-function showNotes(){
+function showNotes() {
     notesContainer.innerHTML = localStorage.getItem('notes');
 }
 showNotes();
 
-function updateStorage(){
+function updateStorage() {
     localStorage.setItem('notes', notesContainer.innerHTML);
 }
 
-createBtn.addEventListener('click', () =>{
+createBtn.addEventListener('click', () => {
     let inputBox = document.createElement('p')
     let img = document.createElement('img')
     inputBox.className = "input-box";
@@ -21,17 +21,23 @@ createBtn.addEventListener('click', () =>{
     notesContainer.appendChild(inputBox).appendChild(img);
 })
 
-notesContainer.addEventListener('click', function(e){
-    if(e.target.tagName === "IMG"){
+notesContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
         updateStorage();
     }
-    else if(e.target.tagName === "P"){
+    else if (e.target.tagName === "P") {
         notes = document.querySelectorAll(".input-box")
-        notes.forEach(nt=>{
-            nt.onkeyup = function(){
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
                 updateStorage();
             }
         })
+    }
+})
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
